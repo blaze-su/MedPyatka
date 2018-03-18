6416cb92a557d4b6b4b1f61bf300db4203c77692
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2018-03-19 00:19:11 +0200

    19.03.18

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,74 @@
 ;
+var $ = document;
+
 window.onload = () => {
-  console.log('1');
+  if ($.getElementsByClassName('control-sort')) listingSort($.getElementsByClassName('listing__generated-container')[0]);
+  if ($.getElementsByClassName('listing-filter')) listingFilter();
+  if ($.getElementsByClassName('search')) search($.getElementsByClassName('listing')[0]);
+};
+
+var listingSort = block => {
+  var option = Array.from($.getElementsByClassName('control-sort__option'));
+  option.forEach(item => {
+    item.onclick = e => {
+      var select = e.target.parentElement;
+
+      select.classList.toggle('-open');
+
+      if (select.classList.contains('-open')) {
+        select.addEventListener('mouseleave', () => {
+          select.classList.remove('-open');
+          if (block) block.style.marginTop = "0px";
+        })
+      }
+
+      var height = select.offsetHeight + 8;
+      console.log(height);
+      if (block) {
+        if (block.style.marginTop != "0px" && block.style.marginTop != "")
+          block.style.marginTop = "0px";
+        else
+          block.style.marginTop = "calc( -" + height + "px + " + 2.222 + "rem)";
+      }
+    }
+  })
+};
+
+var listingFilter = () => {
+  var option = Array.from($.getElementsByClassName('listing-filter__item-header'));
+  option.forEach(item => {
+    item.onclick = e => {
+      var item = e.target;
+      while (item.parentElement.classList.contains('listing-filter__item') == false) item = item.parentElement;
+      item = item.parentElement;
+      item.classList.toggle('-open');
+    }
+  })
+};
+
+var search = block => {
+  var option = Array.from($.getElementsByClassName('search-form__option'));
+  option.forEach(item => {
+    item.onclick = e => {
+      var select = e.target.parentElement;
+      if (select.classList.contains('-open')) {
+        select.children[0].innerHTML = e.target.innerHTML;
+      }
+      select.classList.toggle('-open');
+      
+      select.addEventListener('mouseleave', () => {
+        select.classList.remove('-open');
+        if (block) block.style.marginTop = "0px";
+      })
+      
+      var height = select.offsetHeight - 11;
+
+      if (block) {
+        if (block.style.marginTop != "0px" && block.style.marginTop != "")
+          block.style.marginTop = "0px";
+        else
+          block.style.marginTop = "calc( -" + height + "px + " + 1.667 + "rem)";
+      }
+    }
+  })
 };
