2d960c66418e86053e125bc4fe93ddc0b7076eaf
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2018-03-23 00:15:35 +0200

    23,03,2018

--- source/js/app.js
+++ source/js/app.js
@@ -2,13 +2,14 @@
 var $ = document;
 
 window.onload = () => {
-  if ($.getElementsByClassName('control-sort')) listingSort($.getElementsByClassName('listing__generated-container')[0]);
-  if ($.getElementsByClassName('listing-filter')) listingFilter();
-  if ($.getElementsByClassName('search')) search($.getElementsByClassName('listing')[0]);
+  if ($.querySelector('.control-sort')) listingSort($.querySelector('.listing__generated-container'));
+  if ($.querySelector('.listing-filter')) listingFilter();
+  if ($.querySelector('.search')) search($.querySelector('.listing'));
+  if ($.querySelector('.cart__grid-overview')) cartGalery();
 };
 
 var listingSort = block => {
-  var option = Array.from($.getElementsByClassName('control-sort__option'));
+  var option = $.querySelectorAll('.control-sort__option');
   option.forEach(item => {
     item.onclick = e => {
       var select = e.target.parentElement;
@@ -23,7 +24,6 @@ var listingSort = block => {
       }
 
       var height = select.offsetHeight + 8;
-      console.log(height);
       if (block) {
         if (block.style.marginTop != "0px" && block.style.marginTop != "")
           block.style.marginTop = "0px";
@@ -35,7 +35,7 @@ var listingSort = block => {
 };
 
 var listingFilter = () => {
-  var option = Array.from($.getElementsByClassName('listing-filter__item-header'));
+  var option = $.querySelectorAll('.listing-filter__item-header');
   option.forEach(item => {
     item.onclick = e => {
       var item = e.target;
@@ -47,7 +47,7 @@ var listingFilter = () => {
 };
 
 var search = block => {
-  var option = Array.from($.getElementsByClassName('search-form__option'));
+  var option = $.querySelectorAll('.search-form__option');
   option.forEach(item => {
     item.onclick = e => {
       var select = e.target.parentElement;
@@ -71,4 +71,32 @@ var search = block => {
       }
     }
   })
+};
+
+var cartGalery = () => {
+  var main = $.querySelector('.cart__photo');
+  var preview = $.querySelectorAll('.cart__preview-item');
+
+  preview.forEach(item => {
+    if (item.childNodes[0].getAttribute('src') === main.getAttribute('src')) {
+      item.classList.add('-active');
+      return false;
+    }
+  })
+
+  var _active = el => {
+    $.querySelector('.cart__preview-item.-active').classList.remove('-active');
+    el.classList.add('-active');
+  }
+
+  $.querySelector('.cart__preview-list').onclick = e => {
+    if (e.target.classList.contains('cart__preview-item')) {
+      main.setAttribute('src', e.target.childNodes[0].getAttribute('src'));
+      _active(e.target);
+    }
+    if (e.target.classList.contains('cart__preview')) {
+      main.setAttribute('src', e.target.getAttribute('src'));
+      _active(e.target.parentElement);
+    }
+  }
 };
