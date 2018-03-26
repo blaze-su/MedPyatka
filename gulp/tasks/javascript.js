8cb582d96c41bc5439cb521684572d233c6464d5
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2018-03-26 16:02:45 +0300

    26,03,2018

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -4,12 +4,18 @@ let uglify = require('gulp-uglify-es').default;
 let babel = require('gulp-babel');
 
 module.exports = () => {
-    $.gulp.task('copy:javascripts', () => {
-        return $.gulp.src('./source/js/*.js', { since: $.gulp.lastRun('copy:javascripts') })
-            .pipe(babel({
-                presets: ['env']
-            }))
-            .pipe(uglify())
-            .pipe($.gulp.dest($.config.root + '/assets/js'));
-    });
+	$.gulp.task('copy:javascripts', () => {
+		return $.gulp.src('./source/js/app.js', { since: $.gulp.lastRun('copy:javascripts') })
+			.pipe(babel({
+				presets: ['env']
+			}))
+			.pipe(uglify())
+			.pipe($.gulp.dest($.config.root + '/assets/js')),
+			$.gulp.src('./source/js/app-pyatka.js', { since: $.gulp.lastRun('copy:javascripts') })
+				.pipe(babel({
+					presets: ['env']
+				}))
+				.pipe(uglify())
+				.pipe($.gulp.dest($.config.root + '/assets/js'));
+	});
 };
--- gulp/tasks/js.process.js
+++ gulp/tasks/js.process.js
@@ -4,13 +4,18 @@ let uglify = require('gulp-uglify-es').default;
 let babel = require('gulp-babel');
 
 module.exports = () => {
-    $.gulp.task('js:process', () => {
-        return $.gulp.src($.path.app)
-            .pipe($.gp.concat('app.js'))
-            .pipe(babel({
-                presets: ['env']
-            }))
-            .pipe(uglify())
-            .pipe($.gulp.dest($.config.root + '/assets/js'))
-    })
+	$.gulp.task('js:process', () => {
+		return $.gulp.src('./source/js/app.js')
+			.pipe(babel({
+					presets: ['env']
+			}))
+			.pipe(uglify())
+			.pipe($.gulp.dest($.config.root + '/assets/js')),
+			$.gulp.src('./source/js/app-pyatka.js')
+				.pipe(babel({
+					presets: ['env']
+				}))
+				.pipe(uglify())
+				.pipe($.gulp.dest($.config.root + '/assets/js'));
+	})
 };
